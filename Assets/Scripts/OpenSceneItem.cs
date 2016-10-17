e305fc45d6f40bb8deb27b7deacdcd9b104ed709
Martin Long <tincholong12@gmail.com>
2016-10-17 19:03:42 -0300

    Primer commit

---
+++ Assets/Scripts/OpenSceneItem.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRStandardAssets.Utils;
+using UnityEngine.SceneManagement;
+
+public class OpenSceneItem : MonoBehaviour {
+
+    [SerializeField]
+    private string m_SceneToLoad;
+    [SerializeField]
+    private VRInteractiveItem m_InteractiveItem;
+
+
+    private void OnEnable()
+    {
+        m_InteractiveItem.OnClick += HandleClick;
+    }
+
+
+    private void OnDisable()
+    {
+        m_InteractiveItem.OnClick -= HandleClick;
+    }
+
+    //Handle the Click event
+    private void HandleClick()
+    {
+        SceneManager.LoadScene(m_SceneToLoad, LoadSceneMode.Single);
+    }
+}
